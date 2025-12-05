7107b3c5b9b371af9f9c07ee85350ec5d9bc6e97
nadye <fatmami2002@gmail.com>
2025-12-04 20:07:28 -0800

    architec

---
+++ carte-des-talents/src/app/features/talents-map/talents-map.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TalentsMapRoutingModule } from './talents-map-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TalentsMapRoutingModule
+  ]
+})
+export class TalentsMapModule { }
